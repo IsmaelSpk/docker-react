8ad5ed8e0a83ee8e676213915fae1fba80a1aeae
Ismael J. Silveira <ismaejs88@gmail.com>
2019-09-23 19:38:55 -0300

    Initial Work

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Docker!!!
+          Changed!
         </a>
       </header>
     </div>
